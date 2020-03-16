37a79b664142056ff976aeddc54132717be826ee
Nikita Frolov <nf@mkmks.org>
2020-03-16 11:11:31 +0100

    metals

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,13 +1,16 @@
 {
   allowUnfree = true;
-  
+
   packageOverrides = pkgs : {
                           dwm = pkgs.dwm.override {
                             patches = [ ./dwm-config.diff ];
-                     	  };
+                     	    };
                           st = pkgs.st.override {
-                          patches = [ ./st-config.diff ];
-                     };
+                            patches = [ ./st-config.diff ];
+                          };
+                          idea-community = pkgs.jetbrains.idea-community.override {
+                            jdk = pkgs.openjdk11;
+                          };
   };
 
 }
