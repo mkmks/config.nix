a206f346c5d21403cfa8974788a0a9f476cdf084
Nikita Frolov <nf@mkmks.org>
2020-03-26 21:31:28 +0100

    moved more user packages to home.nix

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -1,8 +1,16 @@
 {pkgs, ...}:
 
-#with import <nixpkgs> {};
+with pkgs;
+
+#let
+#  unstableTarball = fetchTarball #https://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz;
+#  unstable = import unstableTarball {
+#               config = config.nixpkgs.config;
+#             };
+#in
 
 {
+  
   accounts.email = {
     certificatesFile = "/etc/ssl/certs/ca-certificates.crt";
     maildirBasePath = "Mail";
@@ -31,15 +39,15 @@
     enable = true;
     font = {
       name = "Dejavu Sans 9";
-      package = pkgs.dejavu_fonts;
+      package = dejavu_fonts;
     };
     iconTheme = {
       name = "Adwaita";
-      package = pkgs.gnome3.adwaita-icon-theme;
+      package = gnome3.adwaita-icon-theme;
     };
     theme = {
       name = "Adwaita";
-      package = pkgs.gnome3.adwaita-icon-theme;
+      package = gnome3.adwaita-icon-theme;
     };
     gtk2.extraConfig = "gtk-key-theme-name = \"Emacs\"";
     gtk3.extraConfig = {
@@ -47,9 +55,66 @@
       gtk-key-theme-name = "Emacs";
     };
   };
+
+  home = {
+    packages = [
+      # desktop-brands
+      libreoffice
+      skypeforlinux
+      spotify
+      steam
+      tdesktop
+      
+      # desktop-cli
+      ncmpcpp
+      sdcv
+
+      # base
+      mc
+      mg
+      silver-searcher
+
+      # net
+      lftp
+      mu
+      nmap      
+
+      # img
+      pkgs.exif
+      exiftool
+      fdupes      
+      pkgs.imagemagick
+      pdftk
+      xfig       
+    ];
+    
+    sessionVariables = {
+      EDITOR = "emacsclient -ct";
+      ALTERNATIVE_EDITOR = "mg -n";
+      SDCV_PAGER = "less -R";
+      MOZ_ENABLE_WAYLAND = 1;
+    };    
+  };  
   
   programs = {
 
+    termite = {
+      enable = true;
+
+      cursorBlink = "off";
+      scrollbar = "off";
+      
+      clickableUrl = true;
+      dynamicTitle = true;
+
+      browser = "firefox";
+
+      foregroundColor = "white";
+      backgroundColor = "black";
+      cursorColor = "#cccccc";
+    };
+
+    
     fish = {
       enable = true;
 
@@ -93,7 +158,13 @@ gpg-connect-agent -q updatestartuptty /bye > /dev/null
     mbsync.enable = true;
     msmtp.enable = true;
     
+#    firefox = {
+#      enable = true;
+#      package = firefox-beta-bin;
+#    };
+    
     feh.enable = true;
+    gpg.enable = true;
     
     mpv = {
       enable = true;
@@ -101,42 +172,57 @@ gpg-connect-agent -q updatestartuptty /bye > /dev/null
         gpu-context = "wayland";
       };
     };
-    
-    termite = {
+
+    tmux = {
       enable = true;
 
-      cursorBlink = "off";
-      scrollbar = "off";
-      
-      clickableUrl = true;
-      dynamicTitle = true;
+      extraConfig = ''
+        set -g mouse on
+        set -g prefix C-x
+        bind-key C-x send-prefix
+        unbind-key x
+        bind-key k confirm-before -p "kill-pane #P? (y/n)" kill-pane
 
-      browser = "${pkgs.firefox}/firefox";
-
-      foregroundColor = "white";
-      backgroundColor = "black";
-      cursorColor = "#cccccc";
+        set -g renumber-windows on
+        set -g set-titles on
+        set -g set-titles-string "[#I] #T"
+        set -g status on
+        set -g status-position top
+        set -g status-left ""
+        set -g status-right ""
+      '';
     };
-
+    
     zathura = {
       enable = true;
       options = {
-        font = "Monospace 8";
+        font = "Monospace 9";
         window-title-basename = true;
         window-title-page = true;
         guioptions = "";
       };
     };
+
+#    vscode.enable = true;
   };
 
   services = {
+    gnome-keyring.enable = true;
+    
+    gpg-agent = {
+      enable = true;
+      enableSshSupport = true;
+      pinentryFlavor = "gnome3";
+      defaultCacheTtl = 86400;
+      defaultCacheTtlSsh = 86400;
+      maxCacheTtl = 604800;
+      maxCacheTtlSsh = 604800;
+    };
+    
+    mbsync.enable = true;
     udiskie.enable = true;
   };
 
-  systemd.user.sessionVariables = {
-    EDITOR = "emacsclient -cn";
-    ALTERNATIVE_EDITOR = "mg -n";
-    SDCV_PAGER = "less -R";
-  };
+  systemd.user.startServices = true;
   
 }
