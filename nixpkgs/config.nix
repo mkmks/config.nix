4395b5126a8dde05f0aae07c9e153880c999b7d6
Nikita Frolov <nf@mkmks.org>
2020-03-31 22:05:10 +0200

    removed references to deleted patches

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -2,12 +2,6 @@
   allowUnfree = true;
 
   packageOverrides = pkgs : {
-                          dwm = pkgs.dwm.override {
-                            patches = [ ./dwm-config.diff ];
-                     	    };
-                          st = pkgs.st.override {
-                            patches = [ ./st-config.diff ];
-                          };
                           idea-community = pkgs.jetbrains.idea-community.override {
                             jdk = pkgs.openjdk11;
                           };
