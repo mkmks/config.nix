d2ac3bd3a9d4fe4fadd3389cf1fab9a2b2d9f508
Nikita Frolov <nf@mkmks.org>
2021-04-02 17:05:53 +0200

    fish configuration nixified

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -265,49 +265,47 @@ switch $argv[1]
 end
 sdcv -0c -u $dict $argv[2]          
         '';
+
+        my_fish_prompt = ''
+set -l nix_shell_info (
+  if test -n "$IN_NIX_SHELL"
+    echo -n "<nix-shell> "
+  end
+)
+echo -n -s $nix_shell_info '$ '
+        '';
+
+        fish_prompt = ''
+# Remove the trailing newline from the original prompt. This is done
+# using the string builtin from fish, but to make sure any escape codes
+# are correctly interpreted, use %b for printf.
+printf "%b" (string join "\n" (my_fish_prompt))
+vterm_prompt_end
+'';
+
+        fish_title = ''
+hostname
+echo ":"
+pwd
+        '';
+
+        vterm_printf = ''
+if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end 
+  # tell tmux to pass the escape sequences through
+  printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+else if string match -q -- "screen*" "$TERM"
+       # GNU screen (screen, screen-256color, screen-256color-bce)
+       printf "\eP\e]%s\007\e\\" "$argv"
+     else
+       printf "\e]%s\e\\" "$argv"
+     end
+        '';
+
+        vterm_prompt_end = "vterm_printf '51;A'(whoami)'@'(hostname)':'(pwd)";
       };
       
       interactiveShellInit = ''
 set fish_greeting
-
-function fish_prompt
-  set -l nix_shell_info (
-    if test -n "$IN_NIX_SHELL"
-      echo -n "<nix-shell> "
-    end
-  )
-  echo -n -s $nix_shell_info '$ '
-end
-
-function fish_title
-    hostname
-    echo ":"
-    pwd
-end
-
-function vterm_printf;
-    if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end 
-        # tell tmux to pass the escape sequences through
-        printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
-    else if string match -q -- "screen*" "$TERM"
-        # GNU screen (screen, screen-256color, screen-256color-bce)
-        printf "\eP\e]%s\007\e\\" "$argv"
-    else
-        printf "\e]%s\e\\" "$argv"
-    end
-end
-
-function vterm_prompt_end;
-    vterm_printf '51;A'(whoami)'@'(hostname)':'(pwd)
-end
-functions --copy fish_prompt vterm_old_fish_prompt
-function fish_prompt --description 'Write out the prompt; do not replace this. Instead, put this at end of your file.'
-    # Remove the trailing newline from the original prompt. This is done
-    # using the string builtin from fish, but to make sure any escape codes
-    # are correctly interpreted, use %b for printf.
-    printf "%b" (string join "\n" (vterm_old_fish_prompt))
-    vterm_prompt_end
-end
 
 if [ "$INSIDE_EMACS" = 'vterm' ]
     function clear
