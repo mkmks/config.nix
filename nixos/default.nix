ab17211d8b3947cae90fad0786a59bc42c3a9af0
Nikita Frolov <nf@mkmks.org>
2024-11-26 17:46:49 +0100

    correct IOG cache public key

--- nixos/default.nix
+++ nixos/default.nix
@@ -6,7 +6,7 @@
       max-jobs = 8;
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.iog.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+        "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       ];
       trusted-substituters = [
         "https://nix-community.cachix.org"
